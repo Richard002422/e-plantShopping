3d2dc51f008468483031da99369a23079c5bf8f8
Richard002422 <perezmotaricardo162@gamil.com>
2025-12-28 22:14:31 -0500

    Implement cart reducers: addItem, removeItem, updateQuantity

--- src/store.js
+++ src/store.js
@@ -1,10 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./CartSlice";
+// Importar la función configureStore desde Redux Toolkit
+import { configureStore } from '@reduxjs/toolkit';
 
+// Importar el reducer del carrito desde CartSlice
+import cartReducer from './CartSlice';
+
+// Crear la tienda de Redux usando configureStore
 const store = configureStore({
+  // Definir el objeto de reducers raíz
   reducer: {
+    // 'cart' es el nombre del slice en el store
+    // y es gestionado por cartReducer
     cart: cartReducer,
   },
 });
 
+// Exportar la tienda para usarla en toda la aplicación
+// (por ejemplo, en <Provider store={store}>)
 export default store;
